d904e90d7d3014dd942742f807cb0d31fadb1cda
Andrey <andrzejsew@gmail.com>
2017-01-21 13:35:07 +0000

    Build a Multiplication Table Generator

---
+++ 9.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+
+def multiplication_table num
+  hash = Hash.new
+  
+  1.upto(num).each do |i|
+    products = []
+    
+    (1..10).each do |e|
+      products.push(e * i) 
+    end 
+    
+    hash[i] = products
+  end
+  
+  hash
+end
+
+describe 'Multiplication Table' do
+  it 'Creates a hash based multiplication table' do
+    hash = {
+      1=>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      2=>[2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+      3=>[3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+    }
+    
+    expect(multiplication_table 3).to eq(hash)
+  end
+end
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Practice Programming Techniques
 6. Create an Immutable Array
 7. Word Counting and Reporting
 8. Finding Leap Years
+9. Build a Multiplication Table Generator
